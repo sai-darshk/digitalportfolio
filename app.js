b4ff2061e659f90d1d5ec6fef9284b6d030c664c
sai-darshk <saidarshk@gmail.com>
2023-03-06 16:13:46 -0600

    redid website

--- app.js
+++ app.js
@@ -1,31 +1,17 @@
 const hamburger = document.querySelector(".hamburger");
-const navMenu = document.querySelector(".nav-menu");
+const navLinks = document.querySelector(".nav-links");
+const links = document.querySelectorAll(".nav-links li");
 
-hamburger.addEventListener("click", mobileMenu);
+hamburger.addEventListener("click", () => {
+  navLinks.classList.toggle("nav-active");
 
-function mobileMenu() {
-    hamburger.classList.toggle("active");
-    navMenu.classList.toggle("active");
-}
+  links.forEach((link, index) => {
+    if (link.style.animation) {
+      link.style.animation = "";
+    } else {
+      link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 0.5}s`;
+    }
+  });
 
-const navLink = document.querySelectorAll(".nav-link");
-
-navLink.forEach(n => n.addEventListener("click", closeMenu));
-
-function closeMenu() {
-    hamburger.classList.remove("active");
-    navMenu.classList.remove("active");
-}
-
-const carousel = document.querySelector('.carousel');
-const carouselContainer = document.querySelector('.carousel-container');
-const carouselItems = carousel.querySelectorAll('span');
-const itemHeight = carouselContainer.offsetHeight;
-let currentItemIndex = 0;
-
-function updateCarousel() {
-  currentItemIndex = (currentItemIndex + 1) % carouselItems.length;
-  carousel.style.transform = `translateY(-${currentItemIndex * itemHeight}px)`;
-}
-
-setInterval(updateCarousel, 2000);
+  hamburger.classList.toggle("toggle");
+});
