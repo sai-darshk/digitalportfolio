fa79ac380e5dc914fc79e0b080a3f5f5c9df7c77
sai-darshk <saidarshk@gmail.com>
2023-03-06 19:22:29 -0600

    Major website updates

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const hamburger = document.querySelector(".hamburger");
 const navLinks = document.querySelector(".nav-links");
 const links = document.querySelectorAll(".nav-links li");
+const links1 = document.querySelectorAll(".nav-links li");
 
 hamburger.addEventListener("click", () => {
   navLinks.classList.toggle("nav-active");
@@ -14,4 +15,15 @@ hamburger.addEventListener("click", () => {
   });
 
   hamburger.classList.toggle("toggle");
+});
+
+// select all elements with the fade-in class
+const elements = document.querySelectorAll('.fade-in');
+
+// loop through all elements
+elements.forEach(element => {
+  // add the fade-in-animation class after a short delay
+  setTimeout(() => {
+    element.classList.add('fade-in-animation');
+  }, 500);
 });
